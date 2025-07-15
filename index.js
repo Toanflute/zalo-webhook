32cae1df2eed0710fe7089c13530fa9eca3e1f91
Toanflute <gavanghd@gmail.com>
2025-07-15 19:18:37 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const fetch = require("node-fetch");
 const app = express();
+
 app.use(express.json());
+app.use(express.static('public')); // <-- Quan trọng để Zalo đọc file xác thực!
 
 app.post("/", async (req, res) => {
     const data = req.body;
@@ -11,7 +13,7 @@ app.post("/", async (req, res) => {
     const message = data.message.text || "";
     const time = new Date().toISOString();
 
-    await fetch("https://script.google.com/macros/s/AKfycbxwKOOAr__ljV6n2LrLYWIvaclFKNYBw-CuOiKv_Tjz1gMx6LWrjGhF4V8It7zfTj0ieA/exec", { // Dán URL Apps Script thật
+    await fetch("https://script.google.com/macros/s/AKfycbxwKOOAr__ljV6n2LrLYWIvaclFKNYBw-CuOiKv_Tjz1gMx6LWrjGhF4V8It7zfTj0ieA/exec", {
         method: "POST",
         body: JSON.stringify({ zalo_id, message, time }),
         headers: { "Content-Type": "application/json" }
@@ -21,4 +23,6 @@ app.post("/", async (req, res) => {
 });
 
 app.get("/", (req, res) => res.send("OK"));
+
 app.listen(process.env.PORT || 8080);
+
